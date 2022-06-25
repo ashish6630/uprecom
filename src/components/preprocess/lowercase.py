87bc95fe76eea2fcfcb6366fb72e8ab9e09144fe
ashish6630 <ashish6630@gmail.com>
2022-06-25 11:49:49 +0200

    Setting up code coverage for repo

---
+++ src/components/preprocess/lowercase.py
@@ -0,0 +1,9 @@
+
+
+class LowercaseFilter:
+    def __init__(self):
+        ...
+
+    def process(self, text: str) -> str:
+        """Returns lower case of text."""
+        return text.lower()
--- test/components/preprocess/test_whitespace.py
+++ test/components/preprocess/test_whitespace.py
@@ -1,12 +1,29 @@
+import pytest
+
 from src.components.preprocess.whitespace import WhitespaceFilter
+from pytest_lazyfixture import lazy_fixture
+
+@pytest.fixture
+def whitespacefilter():
+    myfilter = WhitespaceFilter()
+    return myfilter
 
 
-def test_whitespace_scenarios():
-    sentence = "this. \n\n has   too much. \n  \n whitespace"
-    expected_sentence = "this. has too much. whitespace"
-
-    filters = WhitespaceFilter()
-
-    filtered_text = filters.process(sentence)
-
-    assert filtered_text == expected_sentence
+@pytest.mark.parametrize(
+    "text, expected_return, filter_fixture",
+    [
+        (
+            "this text. \n\n has   too much. \n  \n whitespace",
+            "this text. has too much. whitespace",
+            lazy_fixture('whitespacefilter')
+        ),
+        (
+            "Lorem ipsum dolor. \n\n sit amet, consectetur. \n\n adipisici elit",
+            "Lorem ipsum dolor. sit amet, consectetur. adipisici elit",
+            lazy_fixture('whitespacefilter')
+        )
+    ]
+)
+def test_whitespace_scenarios(text, expected_return, filter_fixture):
+    filtered_text = filter_fixture.process(text)
+    assert filtered_text == expected_return
